95431f760d098fd72b9144f7c2eec320ef12cb82
ZamirAmit <zamir.amit@gmail.com>
2023-03-15 11:46:50 +0200

    fix

--- story_content/user.js
+++ story_content/user.js
@@ -2,10 +2,10 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
-      case "6bbtUqJ9SGG":
+      case "6KgBpTykp0G":
         Script1();
         break;
-      case "5imP8Kn4h0Y":
+      case "6HQEu0BAB4N":
         Script2();
         break;
   }
@@ -20,43 +20,42 @@ var OperatorName = player.GetVar('OperatorName');
 var CompanyName = player.GetVar('CompanyName');
 var CustomerName = player.GetVar('CustomerName');
 var greetings = "שלום " +
-    CustomerName +
-    ", שמי " +
-    OperatorName +
-    " ואשמח לתת שירות. ";
-var intro_male = ' הפוליסה שלך מבוססת על החזר הוצאות,' +
-    ' כלומר אתה יכול לפנות לכל מוסד רפואי שנוח ונגיש לך,' +
-    ' לקבל טיפול רפואי ולשלם בעבורו. ';
+  CustomerName +
+  ", שמי " +
+  OperatorName +
+  " ואשמח לתת שירות. ";
+var intro_male = 'הפוליסה שלך מבוססת על החזר הוצאות,' +
+  ' כלומר אתה יכול לפנות לכל מוסד רפואי שנוח ונגיש לך,' +
+  ' לקבל טיפול רפואי ולשלם בעבורו. ';
 var save_receipt_male = ' יש לשמור את הקבלה ואת הדוחות הרפואיים בכדי להגיש תביעה לחברת ' +
-    CompanyName +
-    ' זכאות להחזר תיבדק בהתאם לתנאי הפוליסה.';
+  CompanyName +
+  ' זכאות להחזר תיבדק בהתאם לתנאי הפוליסה.';
 var Conversation = greetings +
-    intro_male +
-    save_receipt_male;
+  intro_male +
+  save_receipt_male;
 player.SetVar('Conversation', Conversation);
-
 }
 
 function Script2()
 {
-  
-/* This code is for FEMALE customer: */
+  /* This code is for FEMALE customer: */
 var player = GetPlayer();
 var OperatorName = player.GetVar('OperatorName');
 var CompanyName = player.GetVar('CompanyName');
 var CustomerName = player.GetVar('CustomerName');
 var greetings = "שלום " +
-    CustomerName +
-    ", שמי " +
-    OperatorName +
-    " ואני אשמח לעזור לך. ";
+  CustomerName +
+  ", שמי " +
+  OperatorName +
+  " ואני אשמח לעזור לך. ";
 var intro_female = ' הפוליסה שלך מבוססת על החזר הוצאות,' +
-    ' כלומר את יכולה לפנות לכל מוסד רפואי שנוח ונגיש לך,' +
-    ' לקבל טיפול רפואי ולשלם בעבורו. ';
-var save_receipt_male = ' יש לשמור את הקבלה ואת הדוחות הרפואיים בכדי להגיש תביעה לחברת ' +
-    CompanyName +
-    ' זכאות להחזר תיבדק בהתאם לתנאי הפוליסה.';
+  ' כלומר את יכולה לפנות לכל מוסד רפואי שנוח ונגיש לך,' +
+  ' לקבל טיפול רפואי ולשלם בעבורו.';
+var save_receipt_female = ' יש לשמור את הקבלה ואת הדוחות הרפואיים בכדי להגיש תביעה לחברת' +
+  CompanyName +
+  ' זכאות להחזר תיבדק בהתאם לתנאי הפוליסה.';
 var Conversation = greetings + intro_female + save_receipt_female;
 player.SetVar('Conversation', Conversation);
+
 }
 
